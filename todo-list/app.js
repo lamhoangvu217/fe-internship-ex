f01517d78b22e38eed25b104c7fb5d50505e08ae
Anhbee2k3 <hiphopneverdieiuemneversai@gmail.com>
2024-06-21 17:23:40 +0700

    neww

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,1 +1,10 @@
-console.log("hello")
+function toggleStrikethrough() {
+    var checkbox = document.getElementById("checkbox");
+    var text = document.getElementById("text");
+
+    if (checkbox.checked) {
+        text.classList.add("strikethrough");
+    } else {
+        text.classList.remove("strikethrough");
+    }
+}
